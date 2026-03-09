40d8465229b369022d8d9613297be71df7a3fbe2
taeyeon <greenjuni7@naver.com>
2026-03-09 09:24:40 +0900

    feat: router 연결

--- gamja/src/App.tsx
+++ gamja/src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+
 function App() {
   return (
     <>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
