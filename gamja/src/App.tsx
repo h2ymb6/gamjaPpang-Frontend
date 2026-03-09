a273e88ef29cff611ad2d3d9a260ca1e24612df9
taeyeon <greenjuni7@naver.com>
2026-03-09 19:19:10 +0900

    style: 전역 reset 설정

--- gamja/src/App.tsx
+++ gamja/src/App.tsx
@@ -5,20 +5,27 @@ import Mypage from "./pages/mypage";
 import Main from "./pages/main";
 import Signup from "./pages/signup";
 import Write from "./pages/write";
+import Header from "./components/common/header";
+import { Reset } from "styled-reset";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/detail" element={<Detail />}></Route>
-          <Route path="/main" element={<Main />}></Route>
-          <Route path="/mypage" element={<Mypage />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/write" element={<Write />}></Route>
-        </Routes>
-      </BrowserRouter>
+      
+        <Reset />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />}></Route>
+            <Route path="/detail" element={<Detail />}></Route>
+            <Route path="/main" element={<Main />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/write" element={<Write />}></Route>
+            <Route path="/head" element={<Header />}></Route>
+          </Routes>
+        </BrowserRouter>
+    
     </>
   );
 }
