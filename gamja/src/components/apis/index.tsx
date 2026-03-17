5aa9a716df581f641d60d1a2ce7fe1b88e81dda8
taeyeon <greenjuni7@naver.com>
2026-03-17 21:53:04 +0900

    feat: api hedaers, timeout 추가

--- gamja/src/components/apis/index.tsx
+++ gamja/src/components/apis/index.tsx
@@ -4,4 +4,10 @@ const BaseURL: string = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   baseURL: BaseURL,
-})
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
