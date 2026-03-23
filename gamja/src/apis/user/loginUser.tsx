1a62e6355d3beb8629a5b24ae1eb55780abc5da3
taeyeon <greenjuni7@naver.com>
2026-03-23 22:19:35 +0900

    refactor: 로그인 데이터 타입 지정 및 토큰 저장

--- gamja/src/apis/user/loginUser.tsx
+++ gamja/src/apis/user/loginUser.tsx
@@ -1,15 +1,25 @@
 import api from "..";
 
-const LoginUser = async (userData: any) => {
-  const { email, password } = userData;
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+const LoginUser = async ({ email, password }: LoginRequest) => {
   try {
     const response = await api.post(`/api/v1/auth/login`, {
       email,
       password,
     });
+    const accessToken = response.data?.accessToken || undefined;
+    const refreshToken = response.data?.refreshToken || undefined;
+
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
     return response.data;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 };
 
