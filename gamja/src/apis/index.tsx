68c8a389f9fb8b99ee96249c96f028abac673ac9
taeyeon <greenjuni7@naver.com>
2026-04-01 21:51:49 +0900

    feat: 토큰 만료 시 자동화 구현

--- gamja/src/apis/index.tsx
+++ gamja/src/apis/index.tsx
@@ -10,24 +10,45 @@ export const api = axios.create({
   },
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken") || null;
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
 api.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
+  (response) => response.data,
+  async (error) => {
+    const config = error.config;
+
+    if (error.response.status === 401 && !config._retry) {
+      localStorage.removeItem("accessToken");
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      try {
+        const response: any = await api.post(`/api/v1/auth/refresh`, {
+          refreshToken,
+        });
+
+        config._retry = true;
+
+        const newAccessToken: string = response.accessToken;
+        const newRefreshToken: string = response.refreshToken;
+
+        localStorage.setItem("accessToken", newAccessToken);
+        localStorage.setItem("refreshToken", newRefreshToken);
+
+        config.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return api(config);
+      } catch (error) {
+        console.log(error);
+        alert("다시 시도해주세요");
+        throw error;
+      }
+    }
     return Promise.reject(error);
   },
 );
