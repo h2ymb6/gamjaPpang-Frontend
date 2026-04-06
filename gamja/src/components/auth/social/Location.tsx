435282e37046197cb35156c23b042db57aafede5
taeyeon <greenjuni7@naver.com>
2026-04-06 22:27:02 +0900

    fix: param 오류 해결 및 카카오 소셜 로그인 오류 해결

--- gamja/src/components/auth/social/Location.tsx
+++ gamja/src/components/auth/social/Location.tsx
@@ -1,6 +1,4 @@
 export const naverHandleLogin = () => {
-  const stateValue = "naver";
-  localStorage.setItem("state", stateValue);
   const rootUrl = `https://nid.naver.com/oauth2.0/authorize`;
   const options = {
     client_id: import.meta.env.VITE_NAVER_CLIENT_ID,
@@ -10,30 +8,27 @@ export const naverHandleLogin = () => {
     state: "naver",
   };
 
-  const params = `client_id=${options.client_id}&redirect_uri=${options.redirect_uri}&response_type=code&scope=profile_nickname&state=${stateValue}`;
-  window.location.href = `${rootUrl}?${params}`;
+  const queryString = new URLSearchParams(options).toString();
+
+  window.location.href = `${rootUrl}?${queryString}`;
 };
 
 export const googleHandleLogin = () => {
-  const stateValue = "google";
-  localStorage.setItem("state", stateValue);
   const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
   const options = {
     client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
     redirect_uri: import.meta.env.VITE_REDIRECT_URI,
     response_type: "code",
-    scope: "email profile",
+    scope: "openid email profile",
     state: "google",
   };
 
-  const params = `client_id=${options.client_id}&redirect_uri=${options.redirect_uri}&response_type=code&scope=profile_nickname&state=${stateValue}`;
-  window.location.href = `${rootUrl}?${params}`;
+  const queryString = new URLSearchParams(options).toString();
+
+  window.location.href = `${rootUrl}?${queryString}`;
 };
 
 export const kakaoHandleLogin = () => {
-  const stateValue = "kakao";
-  localStorage.setItem("state", stateValue);
-
   const rootUrl = "https://kauth.kakao.com/oauth/authorize";
   const options = {
     client_id: import.meta.env.VITE_KAKAO_CLIENT_ID,
@@ -42,7 +37,7 @@ export const kakaoHandleLogin = () => {
     scope: "profile_nickname",
     state: "kakao",
   };
+  const queryString = new URLSearchParams(options).toString();
 
-  const params = `client_id=${options.client_id}&redirect_uri=${options.redirect_uri}&response_type=code&scope=profile_nickname&state=${stateValue}`;
-  window.location.href = `${rootUrl}?${params}`;
+  window.location.href = `${rootUrl}?${queryString}`;
 };
